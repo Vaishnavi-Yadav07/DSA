45912ef8ab39b30753793a79724b2684fd7b28a6
Vaishnavi-Yadav07 <vaishnaviedu07@gmail.com>
2026-09-08 01:28:50 +0530

    Time: 794 ms (10.10%) | Memory: 60.3 MB (9.29%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,9 +4,7 @@ class Solution {
         Arrays.sort(nums);
         HashSet<List<Integer>>set = new HashSet<>();
         for(int i =0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
+            
             int left = i+1;
             int right = nums.length-1;
             while(left<right){
